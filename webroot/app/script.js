3b0e055d81eb394b366b17587152b8d14a9756df
Marten Hernebring <marten.hernebring@gmail.com>
2021-02-20 18:42:39 +0100

    Added nav meny and for all screens

--- webroot/app/script.js
+++ webroot/app/script.js
@@ -1,19 +1,22 @@
-async function updateFortune() {
-  try{
+async function updateFortune(evt) {
+  try {
     let resp = await fetch('/api/fortune')
-    if(!resp.ok){
+    if (!resp.ok) {
       throw new Error(response.status)
     }
     let json = await resp.json()
     let fortune = document.getElementById('fortune')
     fortune.innerText = json.fortune
     fortune.style.visibility = 'visible'
-  }
-  catch (error) {
+  } catch (error) {
     console.error(error)
   }
 }
 
-let button = document.getElementById('mf')
-button.addEventListener('click', updateFortune)
-updateFortune()
+function setup() {
+  let button = document.getElementById('random')
+  button.addEventListener('click', updateFortune)
+  updateFortune()
+}
+
+window.addEventListener('load', setup)
